722edf0a3e7c9e09f35cbde83c2044da7a80c4ac
Morteza <m.oskuei@gmail.com>
2022-12-01 20:50:47 +0330

    second change

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app=Flask(__name__)
 @app.route('/')
 def up_time_excoino():
     return str(datetime.datetime.now())
-for i in range(2):
-    print(i)
+
 if __name__=="__main__":
    app.run(debug=True) 
 
