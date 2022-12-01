42214a3a38f9fbdd391f9cba5302a8e925e1370a
Morteza <m.oskuei@gmail.com>
2022-12-01 20:49:42 +0330

    first change

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app=Flask(__name__)
 @app.route('/')
 def up_time_excoino():
     return str(datetime.datetime.now())
-
+for i in range(2):
+    print(i)
 if __name__=="__main__":
    app.run(debug=True) 
 
